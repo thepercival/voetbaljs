53c08e2f2d5a5b03416175e8dc9a14ffacf67967
thepercival <coendunnink@gmail.com>
2017-11-20 21:43:51 +0100

    real rest

--- poule.ts
+++ poule.ts
@@ -8,6 +8,8 @@ import { Team } from './team';
 import { Game } from './game';
 
 export class Poule {
+    static readonly classname = 'Poule';
+
     protected id: number;
     protected round: Round;
     protected number: number;
@@ -15,11 +17,9 @@ export class Poule {
     protected places: PoulePlace[] = [];
     protected games: Game[] = [];
 
-    static readonly classname = "Poule";
-
     // constructor
-    constructor( round: Round, number: number = null ){
-        if( number === null){
+    constructor(round: Round, number: number = null) {
+        if (number === null) {
             number = round.getPoules().length + 1;
         }
         this.setRound(round);
@@ -28,17 +28,17 @@ export class Poule {
 
     getId(): number {
         return this.id;
-    };
+    }
 
-    setId( id: number): void {
+    setId(id: number): void {
         this.id = id;
-    };
+    }
 
     getRound(): Round {
         return this.round;
-    };
+    }
 
-    setRound( round: Round): void {
+    setRound(round: Round): void {
         // if( this.round != null ){ // remove from old round
         //     var index = this.round.getPoules().indexOf(this);
         //     if (index > -1) {
@@ -47,33 +47,33 @@ export class Poule {
         // }
         this.round = round;
         this.round.getPoules().push(this);
-    };
+    }
 
     getNumber(): number {
         return this.number;
-    };
+    }
 
-    setNumber( number: number): void {
+    setNumber(number: number): void {
         this.number = number;
-    };
+    }
 
     getName(): string {
         return this.name;
-    };
+    }
 
     setName(name: string): void {
         this.name = name;
-    };
+    }
 
     getPlaces(): PoulePlace[] {
         return this.places;
     }
 
-    getTeams(): Team[]{
-        let teams: Team[] = [];
-        for( let pouleplace of this.getPlaces() ){
+    getTeams(): Team[] {
+        const teams: Team[] = [];
+        for (const pouleplace of this.getPlaces()) {
             const team = pouleplace.getTeam();
-            if( team != null ) {
+            if (team != null) {
                 teams.push(team);
             }
         }
@@ -85,11 +85,11 @@ export class Poule {
     }
 
     getGamesNotStarted(): Game[] {
-        return this.getGames().filter( (gameIt) => gameIt.getState() === Game.STATE_CREATED );
+        return this.getGames().filter((gameIt) => gameIt.getState() === Game.STATE_CREATED);
     }
 
     needsRanking(): boolean {
-        return ( this.getPlaces().length > 2 );
+        return (this.getPlaces().length > 2);
     }
 
     next(): Poule {
@@ -97,47 +97,47 @@ export class Poule {
         return poules[this.getNumber()];
     }
 
-    addPlace( place: PoulePlace ) {
-        if( place.getNumber() <= this.getPlaces().length ) {
-            throw new Error('de pouleplek kan niet toegevoegd worden, omdat het nummer van de plek kleiner is dan het aantal huidige plekken');
+    addPlace(place: PoulePlace) {
+        if (place.getNumber() <= this.getPlaces().length) {
+            throw new Error('pouleplek kan niet toegevoegd worden, omdat het nummer van de plek kleiner is dan het aantal huidige plekken');
         }
-        place.setPoule( this );
-        this.getPlaces().push( place );
+        place.setPoule(this);
+        this.getPlaces().push(place);
     }
 
-    removePlace( place: PoulePlace ): boolean {
-        var index = this.places.indexOf(place);
-        if (index == -1 ) {
+    removePlace(place: PoulePlace): boolean {
+        const index = this.places.indexOf(place);
+        if (index === -1) {
             return false;
         }
         this.places.splice(index, 1);
         place.setPoule(null);
-        this.places.forEach( function( placeIt ){
-            if( placeIt.getNumber() > place.getNumber() ){
-                placeIt.setNumber( placeIt.getNumber() - 1 );
+        this.places.forEach(function (placeIt) {
+            if (placeIt.getNumber() > place.getNumber()) {
+                placeIt.setNumber(placeIt.getNumber() - 1);
             }
         });
         place.setNumber(null);
         return true;
     }
 
-    movePlace( place: PoulePlace, toNumber: number ) {
-        if( toNumber > this.places.length ){
+    movePlace(place: PoulePlace, toNumber: number) {
+        if (toNumber > this.places.length) {
             toNumber = this.places.length;
         }
-        if( toNumber < 1 ){
+        if (toNumber < 1) {
             toNumber = 1;
         }
 
         // find index of place with same number
-        const foundPlace = this.places.find( function( pouleplaceIt ) {
-            return toNumber == pouleplaceIt.getNumber()
+        const foundPlace = this.places.find(function (pouleplaceIt) {
+            return toNumber === pouleplaceIt.getNumber();
         });
 
         // remove item
         {
             const index = this.places.indexOf(place);
-            if (index == -1) {
+            if (index === -1) {
                 return;
             }
             this.places.splice(index, 1);
@@ -152,8 +152,8 @@ export class Poule {
 
         // update numbers from foundPlace
         let number = 1;
-        this.places.forEach( function( poulePlaceIt ){
-            poulePlaceIt.setNumber( number++ );
+        this.places.forEach(function (poulePlaceIt) {
+            poulePlaceIt.setNumber(number++);
         });
 
         return true;
